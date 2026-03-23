73f140add432ccdfe5bb4d35bee6e2c8dc66abb0
RushikeshD1 <141423104+RushikeshD1@users.noreply.github.com>
2026-03-23 17:11:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async (e) => {
+	e.preventDefault();
+	const desc = document.getElementById('text').value.trim();
+	const timeDelay = document.getElementById('delay').value;
+
+	if(desc === ""){
+		return;
+	}
+	
+	await sleepTime(timeDelay);
+
+	document.getElementById("output").innerHTML = `${desc}`;
+})
+
+const sleepTime = (del = 0) => {
+	return new Promise((res, rej) => {
+		setTimeout(res, del)
+	})
+}
